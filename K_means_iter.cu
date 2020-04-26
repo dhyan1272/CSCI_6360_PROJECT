7077c15ededf8d5527842c0275676190a2f212d1
joshia5 <joshia5@rpi.edu>
2020-04-26 01:45:24 -0400

    working with error in centroid calculation for k=2

--- K_means_iter.cu
+++ K_means_iter.cu
@@ -10,30 +10,30 @@
 #define MAX_ITERS 1  //NUMBER OF ITERATIONS
 #define blockSize 32
 
-double *num=NULL;
-double *centroids_c=NULL;
-int *idx=NULL;
-double *sum=NULL;
-int *count=NULL;
+double *num = NULL;
+double *centroids_c = NULL;
+int *idx = NULL;
+double *sum = NULL;
+int *count = NULL;
+double *num_in_clust = NULL;
+int *count_in_clust = NULL;
 
 __global__
-void findclosestcentroids(double* num, double* centroids_c, int* idx){
-
+void findclosestcentroids(double* num, double* centroids_c, int* idx, double* num_in_clust, int* count_in_clust){
 	int index=blockIdx.x*blockDim.x+threadIdx.x;
 	int stride=blockDim.x*gridDim.x;
 	int offset=0; //offset keeps track if the same thread (number enters the loop the next time, as the thread id will be same)
 	for(int i=index; i<X*Y; i+=stride){
-		
 		int x=index+offset*stride;
 		int j, l, min_ind; 
-		double sum, dist[K],min_dist;
+		double sum_l, dist[K],min_dist;
 		
 		for (j=0;j<K;j++){
-			sum=0;
+			sum_l=0;
 			for (l=0;l<Y;l++){
-					sum=sum+(*(num+x*Y+l)-*(centroids_c+j*Y+l))*(*(num+x*Y+l)-*(centroids_c+j*Y+l));
+					sum_l = sum_l+(*(num+x*Y+l)-*(centroids_c+j*Y+l))*(*(num+x*Y+l)-*(centroids_c+j*Y+l));
 			}
-			dist[j]=sqrt(sum);
+			dist[j]=sqrt(sum_l);
 		}
 		min_dist=dist[0];
 		min_ind=0;
@@ -43,16 +43,20 @@ void findclosestcentroids(double* num, double* centroids_c, int* idx){
 				min_ind=j;
 			}
 		}
-		*(idx+x)=min_ind;
+		*(idx+x) = min_ind;
+		for (l=0;l<Y;l++){
+	          *(num_in_clust+(min_ind*X*Y)+x*Y+l) = *(num+x*Y+l);
+	          *(count_in_clust+(min_ind*X*Y)+x*Y+l) = 1;
+		}
 		offset++;
 	}
 }
 
-__global__ void reduce(double *g_idata, double *g_odata, int *g_odata_count, const unsigned int m, const int *idx, const unsigned int cl)
+__global__ void reduce(double *g_idata, int* g_idata_count, double *g_odata, int *g_odata_count, const unsigned int m, const int *idx, const unsigned int cl)
 {
   unsigned int tid = threadIdx.x;
   unsigned int i = blockIdx.x*(blockSize*2) + tid;
-  if (idx[i]==cl) {
+//  if (idx[i]==cl) {
     unsigned int gridSize = blockSize*2*gridDim.x;
     const unsigned int n = X*Y;
     __shared__ double sdata[n];
@@ -62,15 +66,16 @@ __global__ void reduce(double *g_idata, double *g_odata, int *g_odata_count, con
     sdata_count[tid] = 0;
 
     while (i < n) {
-      sdata[tid] += g_idata[(i*Y+m)] + g_idata[(i*Y+m)+blockSize];
-      sdata_count[tid] += 2; 
+      sdata[tid] += *(g_idata+(i*Y+m)) + *(g_idata+(i*Y+m)+blockSize);
+      sdata_count[tid] += *(g_idata_count+(i*Y+m)) + *(g_idata_count+(i*Y+m)+blockSize); 
+      //sdata_count[tid] += 2; 
       i += gridSize;
     }
     __syncthreads();
 
     if (blockSize >= 512) {if (tid < 256) { sdata[tid] += sdata[tid + 256]; sdata_count[tid] += sdata_count[tid + 256] ;} __syncthreads(); }
     if (blockSize >= 256) {if (tid < 128) { sdata[tid] += sdata[tid + 128]; sdata_count[tid] += sdata_count[tid + 128]; } __syncthreads(); }
-    if (blockSize >= 128) {if (tid < 64) { sdata[tid] += sdata[tid +   64]; sdata_count[tid] += sdata_count[tid +   64]; } __syncthreads(); }
+    if (blockSize >= 128) {if (tid < 64) { sdata[tid] += sdata[tid + 64]; sdata_count[tid] += sdata_count[tid + 64]; } __syncthreads(); }
     
     if (tid < 32) {
         if (blockSize >=  64) sdata[tid] += sdata[tid + 32];
@@ -90,10 +95,10 @@ __global__ void reduce(double *g_idata, double *g_odata, int *g_odata_count, con
 
     if (tid == 0) g_odata[blockIdx.x] = sdata[0];
     if (tid == 0) g_odata_count[blockIdx.x] = sdata_count[0];
-  }
+//  }
 }
 
-void computeCentroids(double* num, int* idx, double* centroids, int n_blocks) {
+void computeCentroids(double* num, int* idx, double* centroids, int n_blocks, double* num_in_clust, int* count_in_clust) {
   double sum_g = 0.0;
   int count_g = 0;
   cudaMallocManaged(&sum, sizeof(double)*n_blocks);
@@ -104,7 +109,7 @@ void computeCentroids(double* num, int* idx, double* centroids, int n_blocks) {
 	sum[j] = 0.0;
 	count[j] = 0;
       }
-      reduce <<<n_blocks, blockSize>>> (num, sum, count, m, idx, i);
+      reduce <<<n_blocks, blockSize>>> (&num_in_clust[X*Y*i], &count_in_clust[X*Y*i], sum, count, m, idx, i);
       cudaDeviceSynchronize();
 
       //do blocksum
@@ -113,7 +118,7 @@ void computeCentroids(double* num, int* idx, double* centroids, int n_blocks) {
 	sum_g += sum[j];
 	count_g += count[j];
       }
-      printf("m = %d, sum=%f, count=%d \n", m, sum_g, count_g);
+      printf("k=%d, m=%d, centroid=%f \n", i, m, sum_g/count_g);
       *(centroids+i*Y+m)=sum_g/count_g;
     }
   }
@@ -131,16 +136,13 @@ int main(){
 	double num1;
 	int i, j, n_blocks;
 	n_blocks = (X*Y + blockSize - 1)/blockSize;
-/*
-	if (no_of_threads==(X*Y))
-        n_blocks = (X*Y)/no_of_threads; //calculation of number of blocks baseds on threadscounts and world size
-    else 
-        n_blocks = (X*Y)/no_of_threads+1;;
-*/
+
 	//Initializing CUDA memory 
 	cudaMallocManaged(&num, sizeof(double)*X*Y);
 	cudaMallocManaged(&centroids_c, sizeof(double)*K*Y);
 	cudaMallocManaged(&idx, sizeof(int)*X);
+        cudaMallocManaged(&num_in_clust, sizeof(double)*X*Y*K);
+        cudaMallocManaged(&count_in_clust, sizeof(int)*X*Y*K);
 
 	//Opening file and loading data into CUDA memory.
 	FILE *fp;
@@ -166,14 +168,10 @@ int main(){
 	}
 
 	for (i=0;i<MAX_ITERS;i++){
-//question: why are we passing the global arrays as arguments??, the functions already have the info
-		findclosestcentroids <<<n_blocks, blockSize>>> (num, &centroids_c[0], &idx[0]);
+		findclosestcentroids <<<n_blocks, blockSize>>> (num, &centroids_c[0], &idx[0], &num_in_clust[0], &count_in_clust[0]);
 		cudaDeviceSynchronize();
-		computeCentroids(num, &idx[0], &centroids_c[0], n_blocks);
+		//for (j=0; j<K; ++j) printf("k=%d, count=%d\n", j, count_in_clust[j]);
+		computeCentroids(num, &idx[0], &centroids_c[0], n_blocks, num_in_clust, count_in_clust);
 	}
-	
-	//for (i=0;i<X;i++){
-	//	printf("%d===%d \n",i+1, idx[i]+1);
-	//}
 	return 0;
 }
