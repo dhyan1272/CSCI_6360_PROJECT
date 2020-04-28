10f2daa182955a8baebf03a54685cf9892b15dc0
Abolaji Adesoji <adesojidiekola@gmail.com>
2020-04-28 11:39:22 -0400

    only resolving indentations

--- HW4/parallelio.c
+++ HW4/parallelio.c
@@ -1,11 +1,11 @@
 /*
-* The code has been written by Karan Bhanot, Abolaji Adesoji, Aditya Joshi and Dhyanjyoti Nath.
-*
-* Some function definitions are referenced from
-* sample code provided by Christopher D. Carothers, 
-* provided as part of his class assignment of Parallel Computing 
-* Spring 2020.
-*/
+ * The code has been written by Karan Bhanot, Abolaji Adesoji, Aditya Joshi and Dhyanjyoti Nath.
+ *
+ * Some function definitions are referenced from
+ * sample code provided by Christopher D. Carothers, 
+ * provided as part of his class assignment of Parallel Computing 
+ * Spring 2020.
+ */
 
 // Import required libraries
 #include<stdio.h>
@@ -17,9 +17,9 @@
 typedef unsigned long long ticks;
 
 /*
-* Function that returns the ticks at the momment in time
-* (Taken from the code provided as part of Parallel Computing class)
-*/
+ * Function that returns the ticks at the momment in time
+ * (Taken from the code provided as part of Parallel Computing class)
+ */
 static __inline__ ticks getticks(void)
 {
   unsigned int tbl, tbu0, tbu1;
@@ -34,146 +34,146 @@ static __inline__ ticks getticks(void)
 }
 
 /*
-* main function that handles the MPI operations for writing and reading
-* a file using parallel IO while keeping track of the number of ticks
-*/
+ * main function that handles the MPI operations for writing and reading
+ * a file using parallel IO while keeping track of the number of ticks
+ */
 int main(int argc, char *argv[]) {
 
-	// Variables to track ticks for writing and reading
-	unsigned long long write_start = 0;
-	unsigned long long write_finish = 0;
-	unsigned long long read_start = 0;
-	unsigned long long read_finish = 0;
+  // Variables to track ticks for writing and reading
+  unsigned long long write_start = 0;
+  unsigned long long write_finish = 0;
+  unsigned long long read_start = 0;
+  unsigned long long read_finish = 0;
 
-	// Variables to track MPI rank and total ranks
-	int myrank = 0;
-    int numranks = 0;
+  // Variables to track MPI rank and total ranks
+  int myrank = 0;
+  int numranks = 0;
 
-    // MPI variables for file and status of reading
-	MPI_File fh;
-	MPI_Status status;
+  // MPI variables for file and status of reading
+  MPI_File fh;
+  MPI_Status status;
 
-	// Check if the required number of arguments were provided
-	if( argc != 2 )
-    {
-		printf("The code requires the argument for the size of each blocks e.g. ./io 1024 \n");
-		exit(-1);
-    }
+  // Check if the required number of arguments were provided
+  if( argc != 2 )
+  {
+    printf("The code requires the argument for the block-size e.g. ./io 1024 \n");
+    exit(-1);
+  }
 
-    // Assign value based on the provided value
-    int filesize = atoi(argv[1]);
+  // Assign value based on the provided value
+  int filesize = atoi(argv[1]);
 
-	// Initialize MPI
-    MPI_Init(&argc, &argv);
+  // Initialize MPI
+  MPI_Init(&argc, &argv);
 
-    // Get current rank and total number of ranks
-    MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
-    MPI_Comm_size(MPI_COMM_WORLD, &numranks);
+  // Get current rank and total number of ranks
+  MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
+  MPI_Comm_size(MPI_COMM_WORLD, &numranks);
 
-    // Allocate memory to buffer
-    int* buf = malloc(filesize);
+  // Allocate memory to buffer
+  int* buf = malloc(filesize);
 
-    // Calculate the total count of values in each block to write/read
-    int block_count = filesize/sizeof(int);
+  // Calculate the total count of values in each block to write/read
+  int block_count = filesize/sizeof(int);
 
-    // Set each value to be 1
-    for (int i=0; i<block_count; i++) { buf[i] = 1; }
+  // Set each value to be 1
+  for (int i=0; i<block_count; i++) { buf[i] = 1; }
 
-    /*************************************************
-    **************************************************
-    ***************** WRITING TO FILE ****************
-	**************************************************
-    *************************************************/
+  /*************************************************
+   **************************************************
+   ***************** WRITING TO FILE ****************
+   **************************************************
+   *************************************************/
 
-	// If rank is 0, start calculation using ticks
-    if (myrank == 0) {
-    	write_start = getticks();
-    }
+  // If rank is 0, start calculation using ticks
+  if (myrank == 0) {
+    write_start = getticks();
+  }
 
-    // Open the file in write mode. If it does not exist, create it first
-    MPI_File_open(MPI_COMM_WORLD, "datafile.txt", MPI_MODE_CREATE | MPI_MODE_WRONLY, MPI_INFO_NULL, &fh);
- 
- 	// Write the data for each block in accordance with the rank number
-	// If there are 64 ranks, the order of writing is:
-	// Rank 0 - Block 0
-	// Rank 1 - Block 0
-	// .
-	// .
-	// .
-	// Rank 64 - Block 0
-	// Rank 0 - Block 1
-	// .
-	// .
-	for (int i=0; i < 64; i++) {
-		int write_index = filesize*myrank + filesize*numranks*i; 
-		MPI_File_write_at(fh, write_index, buf, block_count, MPI_INT, &status);
-	}
+  // Open the file in write mode. If it does not exist, create it first
+  MPI_File_open(MPI_COMM_WORLD, "datafile.txt", MPI_MODE_CREATE | MPI_MODE_WRONLY, MPI_INFO_NULL, &fh);
 
-	// Add barrier
-	MPI_Barrier(MPI_COMM_WORLD);
+  // Write the data for each block in accordance with the rank number
+  // If there are 64 ranks, the order of writing is:
+  // Rank 0 - Block 0
+  // Rank 1 - Block 0
+  // .
+  // .
+  // .
+  // Rank 64 - Block 0
+  // Rank 0 - Block 1
+  // .
+  // .
+  for (int i=0; i < 64; i++) {
+    int write_index = filesize*myrank + filesize*numranks*i; 
+    MPI_File_write_at(fh, write_index, buf, block_count, MPI_INT, &status);
+  }
 
-	// Close the file
-    MPI_File_close(&fh);
+  // Add barrier
+  MPI_Barrier(MPI_COMM_WORLD);
 
-    // After all blocks and rank writing, when rank is 0, print the total ticks
-    if (myrank == 0) {
-    	write_finish = getticks();
-    	printf("Time taken to perform write operation: %llu ticks\n", (write_finish - write_start));
-    }
+  // Close the file
+  MPI_File_close(&fh);
 
-    // Free the buffer
-    free(buf);
+  // After all blocks and rank writing, when rank is 0, print the total ticks
+  if (myrank == 0) {
+    write_finish = getticks();
+    printf("Time taken to perform write operation: %llu ticks\n", (write_finish - write_start));
+  }
 
-    // Reinitialize buffer
-    buf = malloc(filesize);
+  // Free the buffer
+  free(buf);
 
-	/*************************************************
-    **************************************************
-    *************** READING FROM FILE ****************
-	**************************************************
-    *************************************************/
+  // Reinitialize buffer
+  buf = malloc(filesize);
 
-    // If rank is 0, start calculation using ticks
-    if (myrank == 0) {
-    	read_start = getticks();
-    }
+  /*************************************************
+   **************************************************
+   *************** READING FROM FILE ****************
+   **************************************************
+   *************************************************/
 
-    // Open the file in write mode. If it does not exist, create it first
-  	MPI_File_open(MPI_COMM_WORLD, "datafile.txt", MPI_MODE_RDONLY, MPI_INFO_NULL, &fh);
+  // If rank is 0, start calculation using ticks
+  if (myrank == 0) {
+    read_start = getticks();
+  }
 
-  	// Read in all the data using various ranks and blocks
-	// If there are 64 ranks, the order of reading is:
-	// Rank 0 - Block 0
-	// Rank 1 - Block 0
-	// .
-	// .
-	// .
-	// Rank 64 - Block 0
-	// Rank 0 - Block 1
-	// .
-	// .
-	for (int i = 0; i < 64; i++) {
-		int read_index = filesize*myrank + filesize*numranks*i; 
-		MPI_File_read_at(fh, read_index, buf, block_count, MPI_INT, &status);
-	}
+  // Open the file in write mode. If it does not exist, create it first
+  MPI_File_open(MPI_COMM_WORLD, "datafile.txt", MPI_MODE_RDONLY, MPI_INFO_NULL, &fh);
 
-	// Add barrier
-	MPI_Barrier(MPI_COMM_WORLD);
+  // Read in all the data using various ranks and blocks
+  // If there are 64 ranks, the order of reading is:
+  // Rank 0 - Block 0
+  // Rank 1 - Block 0
+  // .
+  // .
+  // .
+  // Rank 64 - Block 0
+  // Rank 0 - Block 1
+  // .
+  // .
+  for (int i = 0; i < 64; i++) {
+    int read_index = filesize*myrank + filesize*numranks*i; 
+    MPI_File_read_at(fh, read_index, buf, block_count, MPI_INT, &status);
+  }
 
-	// Close the file
-	MPI_File_close(&fh);
+  // Add barrier
+  MPI_Barrier(MPI_COMM_WORLD);
 
-	// After all blocks and rank writing, when rank is 0, print the total ticks
-    if (myrank == 0) {
-    	read_finish = getticks();
-    	printf("Time taken to perform read operation: %llu ticks\n", (read_finish - read_start));
-    }
+  // Close the file
+  MPI_File_close(&fh);
 
-    // Free the buffer
-    free(buf);
+  // After all blocks and rank writing, when rank is 0, print the total ticks
+  if (myrank == 0) {
+    read_finish = getticks();
+    printf("Time taken to perform read operation: %llu ticks\n", (read_finish - read_start));
+  }
 
-    // Finalize MPI
-	MPI_Finalize();
+  // Free the buffer
+  free(buf);
 
-    return 0;
+  // Finalize MPI
+  MPI_Finalize();
+
+  return 0;
 }
