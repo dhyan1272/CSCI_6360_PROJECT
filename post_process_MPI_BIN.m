3c1db41e09d70e5109f8a162d66c6ed214981bf1
Dhyanjyoti <nathd@rpi.edu>
2020-05-02 19:06:54 -0400

    prints image

--- post_process_MPI_BIN.m
+++ post_process_MPI_BIN.m
@@ -3,7 +3,7 @@ clear all
 close all
 
 %Original Image
-A = double(imread('x2.jpg'));
+A = double(imread('X.jpg'));
 A = A / 255; % Divide by 255 so that all values are in the range 0 - 1
 img_size = size(A);
 
@@ -14,12 +14,23 @@ fclose(fileID);
 X1=reshape(X_bin', 3, img_size(1)*img_size(2)).';
 A2 = reshape(X1, img_size(1), img_size(2), 3);
 
-%Display original image
-subplot(1, 2, 1);
-imagesc(A); 
-title('Original');
 
-% Display compressed image
-subplot(1, 2, 2);
-imagesc(A2)
-title(sprintf('Compressed image'));
+outputImage = imresize(A2, [img_size(1) img_size(2)]);
+imwrite(outputImage , 'compressed.jpg')
+
+
+% outputImage = imresize(A, [img_size(1) img_size(2)]);
+% imwrite(outputImage , 'original.jpg')
+% %Display original image
+% %subplot(1, 2, 1);
+% imagesc(A);
+% 
+% set(gca,'FontSize',20)
+% %title('Original');
+% 
+% % Display compressed image
+% %subplot(1, 2, 2);
+% figure
+% imagesc(A2)
+% set(gca,'FontSize',20)
+% %title(sprintf('Compressed image'));
